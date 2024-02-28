5c71c80579c4770c50ed88d79f31c0780f8a0dc0
msalazar <minoru.salazar@elektra.com.mx>
2024-02-28 16:35:26 -0600

    Se comentan  partes d ecodigo con monggose 2

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    nombre: String,
-    correo: String,
-    edad: Number
-});
+// const userSchema = new mongoose.Schema({
+//     username: String,
+//     password: String,
+//     nombre: String,
+//     correo: String,
+//     edad: Number
+// });
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
